acea0f53ab806dc556ea3fe40a8725f0ef5067aa
Joaquin <joaquin@MacBook-Pro-de-Joaquin.local>
2018-04-15 23:58:56 -0300

    update contract

--- Contracts/contracts.sol
+++ Contracts/contracts.sol
@@ -6,17 +6,52 @@ contract LotteryChain {
       // Events
       event But_ticket(address _user);
       event Transfer(address indexed from, address indexed to, uint256 value);
-
+      mapping(uint => uint) public lista_tickets;
+      mapping (address => uint256) public balanceOf;
+      mapping (address => mapping (address => uint256)) public allowance;
       //Ticket price
       uint public precio;
+      address public bank;
 
       //lottery start day
       uint public dia_inicio;
+      mapping(uint => address[]) public tickets;
 
       //lottery total tickets sell
       uint public total_tickets;
 
-      mapping(uint => uint) public lista_tickets;
+
+    function _transfer(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balanceOf[_from] >= _value);
+        // Check for overflows
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
+        // Save this for an assertion in the future
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        // Subtract from the sender
+        balanceOf[_from] -= _value;
+        // Add the same to the recipient
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+    }
+
+        function transfer(address _to, uint256 _value) public {
+        _transfer(msg.sender, _to, _value);
+    }
+
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
+
 
       function LotteryChain(uint _dia_inicio) {
         precio = 0.001 ether;
@@ -31,25 +66,19 @@ contract LotteryChain {
       function agregar_ticket(uint guess, address _user) private {
 
     }
-      fuction comprar_ticket(adress _from, uint _value) internal {
+      function comprar_ticket(uint guess, address _from, uint _value) internal {
         // check payment is correct
         if (msg.value != precio) throw;
-        // Check if the sender has enough
-        require(balanceOf[_from] >= precio);
-        // Check for overflows
-        require(balanceOf[lottery_address] + _value > balanceOf[lottery_address]);
+
         // Subtract from the sender
-        balanceOf[_from] -= precio;
+        transfer(bank, _value);
+        tickets[guess].push(_from);
+
+        total_tickets += 1;
         // Add the same to the recipient
-        balanceOf[lottery_address] += precio;
-        Transfer(_from, lottery_address, precio);
 
-        insert_bet(guess, msg.sender);
-        Buy_ticket(msg.sender);
+        //insert_bet(guess, msg.sender);
+        //Buy_ticket(msg.sender);
       }
-
-
-
-
 
 }
