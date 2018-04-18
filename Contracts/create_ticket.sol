65300ed6208bf6b7696db4e28f87c90f32f37261
Joaquin <joaquin@MacBook-Pro-de-Joaquin.local>
2018-04-17 23:33:29 -0300

    update

--- Contracts/create_ticket.sol
+++ Contracts/create_ticket.sol
@@ -1,3 +1,7 @@
+pragma solidity ^0.4.2;
+
+contract Lottery {
+
       struct Ticket {
        address adr;
        uint256 number1;
@@ -6,6 +10,10 @@
     
     Ticket[] public tickets;
     uint public total_tickets;
+    uint public ticket_price;
+    uint public betting_period;
+    uint public waiting_period;
+
     
     function Lottery(uint _betting_period, uint _waiting_period) {
       ticket_price = 0.1 ether;
@@ -17,7 +25,7 @@
     
     function create_ticket(uint256 n1, uint256 n2){
       tickets.push(Ticket({
-          msg.sender,
+          adr: msg.sender,
           number1: n1,
           number2: n2
       }));
@@ -28,3 +36,7 @@
     function reset_tickets() private {
       total_tickets = 0;
     }
+    
+
+
+}
